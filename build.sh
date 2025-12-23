9ffc460fadd5138bab8985b3635ea61735f3c615
arnob2112 <ehshanulhq@gmail.com>
2025-12-24 02:08:07 +0600

    fix cffi and remove sudo

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get install -y libcairo2 libpango-1.0-0 libgdk-pixbuf2.0-0 libffi-dev shared-mime-info
 pip install --upgrade pip
 pip install -r requirements.txt
